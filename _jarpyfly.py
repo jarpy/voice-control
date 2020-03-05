2aca0b8247ba7df1797118a22c61973c65bb5cde
Toby McLaughlin <toby@jarpy.net>
2020-03-05 12:27:58 +1100

    Too many changes

--- _jarpyfly.py
+++ _jarpyfly.py
@@ -1,14 +1,22 @@
+# -*- coding: utf-8 -*-
+
 from dragonfly import CompoundRule, Grammar, MappingRule, Integer, Dictation, Function
-from aenea.strict import Key, Text, Pause
+from aenea.strict import Key, Text, Pause                   
 from dragonfly import Key as OriginalKey
+from dragonfly import FocusWindow as LocalFocusWindow
 from aenea import ProxyAppContext as AppContext
 from dragonfly.windows.clipboard import Clipboard
+
 import pyttsx
+import win32gui
+
 speech_engine = pyttsx.init()
 
+global_context = None
 
 emacs_context = AppContext(title="emacs")
-vscode_context = AppContext(title="Code - OSS")
+vscode_linux_context = AppContext(title="Code - OSS")
+vscode_windows_context = AppContext(title="Visual Studio Code")
 gmail_context = AppContext(title="Elastic Mail")
 terminal_context = AppContext(title="termite")
 
@@ -16,29 +24,93 @@ firefox_context = AppContext(executable="firefox")
 chrome_context = AppContext(title="Google Chrome")
 chromium_context = AppContext(title="Chromium")
 browser_context = chrome_context | chromium_context | firefox_context
+vscode_context = vscode_linux_context | vscode_windows_context
+
+nato_alphabet = {
+    'alpha': 'a',
+    'alfa': 'a',
+    'offer': 'a',
+    'of the': 'a',
+    'bravo': 'b',
+    'charlie': 'c',
+    'delta': 'd',
+    'echo': 'e',
+    'foxtrot': 'f',
+    'golf': 'g',
+    'hotel': 'h',
+    'india': 'i',
+    'juliett': 'j',
+    'juliet': 'j',
+    'julian': 'j',
+    'kilo': 'k',
+    'killer': 'k',
+    'lima': 'l',
+    'limo': 'l',
+    'mike': 'm',
+    'november': 'n',
+    'oscar': 'o',
+    'papa': 'p',
+    'popper': 'p',
+    'quebec': 'q',
+    'québec': 'q',
+    'romeo': 'r',
+    'sierra': 's',
+    'sarah': 's',
+    'tango': 't',
+    'uniform': 'u',
+    'victor': 'v',
+    'whiskey': 'w',
+    'x-ray': 'x',
+    'yankee': 'y',
+    'zulu': 'z',
+    'zero': '0',
+    'one': '1',
+    'won': '1',
+    'two': '2',
+    'to': '2',
+    'too': '2',
+    'three': '3',
+    'for': '4',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'niner': '9',
+}
 
 
-def downcase(text):
+def down_case(text):
     "This thing => this thing"
-    return str(text).lower()
+    return text.lower()
 
-def smash(text):
+def smash_case(text):
     "This thing => thisthing"
-    return str(text).lower().replace(' ', '')
+    return text.lower().replace(' ', '')
 
 def kebab_case(text):
     "This thing => this-thing"
-    return str(text).lower().replace(' ', '-')
+    return text.lower().replace(' ', '-')
 
 def snake_case(text):
     "This thing => this_thing"
-    return str(text).lower().replace(' ', '_')
+    return text.lower().replace(' ', '_')
+
+def nato_to_char(text):
+    try:
+        print "Nato: %s" % text
+        return [nato_alphabet[phonetic] for phonetic in text.lower().split()]
+    except KeyError:
+        return ["nato error".split()]  # FIXME: make a noise
+
 
 # Emacs (and vscode)
 emacs_grammar = Grammar("emacs", emacs_context | vscode_context)
 class EmacsRule(MappingRule):
     mapping = {
-        "clang": Key("c-g:5"),
+        "clang": Key("c-g:2"),
+        "yank": Key("c-y"),
         "one window": Key("c-x, 1"),
         "two windows": Key("c-x, 1, c-x, 3"),
         "three windows": Key("c-x, 1, c-x, 3, c-x, 3, c-x, plus"),
@@ -49,7 +121,7 @@ class EmacsRule(MappingRule):
         "done with buffer": Key("c-x, hash"),
         "retro word": Key("a-b"),
         "pro word": Key("a-f"),
-        "start [of] line": Key("c-a"),
+        "kill line": Key("c-k"),
         "kill whole line": Key("c-a, c-a, c-k, c-k"),
         "window right": Key("s-right"),
         "window far right": Key("s-right:4"),
@@ -58,13 +130,20 @@ class EmacsRule(MappingRule):
         "window down": Key("s-down"),
         "window up": Key("s-up"),
         "open line": Key("c-e, a-o"),
+        "save (buffer|file)": Key("c-x, c-s"),
+        "Centre view": Key("c-u, 1, 0, c-l"),
         "(no no|nono)": Key("cs-minus"),
         "(duplicate|dupe|double) (line|region|that)": Key("c-c, d"),
         "(top|start) [of] buffer": Key("as-comma"),
         "(bottom|end) [of] buffer": Key("as-dot"),
+        "start [of] line": Key("c-a"),
+        "end [of] line": Key("c-e"),
         "(search forward|pro search) <text>": Key("c-s") + Text("%(text)s"),
         "(search forward|pro search)": Key("c-s"),
-    }
+        "(mark|set mark)": Key("c-space"),
+        "jump line": Key("j, l"),
+        "jump word": Key("j, w"),
+}
 
     extras = [
         Dictation("text"),
@@ -79,7 +158,7 @@ class FindFileRule(CompoundRule):
 
     def _process_recognition(self, node, extras):
         Key("c-x, c-f").execute()
-        Text(smash(extras["text"])).execute()
+        Text(smash_case(extras["text"])).execute()
         # Key("enter").execute()
 emacs_grammar.add_rule(FindFileRule())
 
@@ -90,7 +169,7 @@ class RecentFileRule(CompoundRule):
 
     def _process_recognition(self, node, extras):
         Key("c-c, f").execute()
-        Text(smash(extras["text"])).execute()
+        Text(smash_case(extras["text"])).execute()
         # Key("enter").execute()
 emacs_grammar.add_rule(RecentFileRule())
 
@@ -101,9 +180,20 @@ class JumpBufferRule(CompoundRule):
 
     def _process_recognition(self, node, extras):
         Key("c-x, b").execute()
-        Text(smash(extras["text"])).execute()
+        Text(smash_case(extras["text"])).execute()
         Key("enter").execute()
 emacs_grammar.add_rule(JumpBufferRule())
+
+
+class JumpWordRule(CompoundRule):
+    spec = "jump word <text>"
+    extras = [Dictation("text")]
+
+    def _process_recognition(self, node, extras):
+        letters = nato_to_char(str(extras["text"]))
+        Key("j, w").execute()
+        Key(', '.join(letters)).execute()
+emacs_grammar.add_rule(JumpWordRule())
 
 
 class MetaXRule(CompoundRule):
@@ -112,21 +202,23 @@ class MetaXRule(CompoundRule):
 
     def _process_recognition(self, node, extras):
         Key("a-x").execute()
-        Text(smash(extras["text"])).execute()
+        Pause("10").execute()
+        Text(smash_case(str(extras["text"]))).execute()
         Key("enter").execute()
 emacs_grammar.add_rule(MetaXRule())
 
 emacs_grammar.load()
 
 # Global
-global_grammar = Grammar("global")
+global_grammar = Grammar("global-grammar", context=global_context)
 
 class DowncaseRule(CompoundRule):
     spec = "(flatten|lower|downcase) <text>"
     extras = [Dictation("text")]
 
     def _process_recognition(self, node, extras):
-        Text(downcase(extras["text"])).execute()
+        t = Text(down_case(str(extras["text"])))
+        t.execute()
 global_grammar.add_rule(DowncaseRule())
 
 
@@ -135,7 +227,8 @@ class SmashRule(CompoundRule):
     extras = [Dictation("text")]
 
     def _process_recognition(self, node, extras):
-        Text(smash(extras["text"])).execute()
+        t = Text(smash_case(str(extras["text"])))
+        t.execute()
 global_grammar.add_rule(SmashRule())
 
 
@@ -144,7 +237,8 @@ class KebabRule(CompoundRule):
     extras = [Dictation("text")]
 
     def _process_recognition(self, node, extras):
-        Text(kebab_case(extras["text"])).execute()
+        t = Text(kebab_case(str(extras["text"])))
+        t.execute()
 global_grammar.add_rule(KebabRule())
 
 
@@ -153,11 +247,33 @@ class SnakeRule(CompoundRule):
     extras = [Dictation("text")]
 
     def _process_recognition(self, node, extras):
-        Text(snake_case(extras["text"])).execute()
+        t = Text(snake_case(str(extras["text"])))
+        t.execute()
 global_grammar.add_rule(SnakeRule())
 
-global_grammar.load()
 
+class NatoRule(CompoundRule):
+    spec = "(nato|type|press|punch|push) <text>"
+    extras = [Dictation("text")]
+
+    def _process_recognition(self, node, extras):
+        print("NatoRule: %s" % str(extras["text"]))
+        letters = nato_to_char(str(extras["text"]))
+        Key(', '.join(letters)).execute()
+global_grammar.add_rule(NatoRule())
+
+
+class LongoptRule(CompoundRule):
+    spec = "longopt <text>"
+    extras = [Dictation("text")]
+
+    def _process_recognition(self, node, extras):
+        t = Text(kebab_case('--' + str(extras["text"])))
+        t.execute()
+
+global_grammar.add_rule(LongoptRule())
+
+global_grammar.load()
 
 
 class GmailRule(MappingRule):
@@ -169,8 +285,8 @@ class GmailRule(MappingRule):
         "oldest": Key("j:100"),
         "newest": Key("k:100"),
         "open": Key("o"),
+        "(expand|show all)": Key(";"),
     }
-
 
 gmail_grammar = Grammar("gmail", gmail_context)
 gmail_grammar.add_rule(GmailRule())
@@ -190,7 +306,12 @@ class BrowserRule(MappingRule):
         "(smaller|zoom out)": Key("cs-minus"),
         "refresh": Key("cs-r"),
         "close tab": Key("c-w"),
-        "tab <n>": Key("a-%(n)d"),
+        "tab <n>": Key("c-%(n)d"),
+        "new window": Key("c-n"),
+        "new tab": Key("c-t"),
+        "browse <text>": Key("c-l") + Text("%(text)s") + Key("enter"),
+        "browse history <text>": Key("c-l") + Text("%(text)s") + Key("down"),
+        "slack channel <text>": Key("c-k") + Text("%(text)s"),
     }
     extras = [
         Dictation("text"),
@@ -218,10 +339,16 @@ terminal_grammar.add_rule(TermiteRule())
 class BashRule(MappingRule):
     mapping = {
         "new session <text>": Text("tmux new-session -t %(text)s") + Key("enter"),
-        "(attach|connect) session <text>": Text("tmux attach -t %(text)s") + Key("enter"),
+        "(attach|connect) session <text>": Text("tmux attach -t %(text)s") + Key("tab, enter"),
         "list sessions": Text("tmux list-sessions") + Key("enter"),
+        "(Rerun last|do again|do it again)": Key("up, enter"),
         "go home": Text("cd ~") + Key("enter"),
         "go source (folder|dir|directory)": Text("cd ~/src/") + Key("enter"),
+        "get pods": Text("kubectl get pods") + ("enter"),
+        "git reset hard": Text("git reset --hard") + Key("enter"),
+        "git pull": Text("git pull") + Key("enter"),
+        "git fetch [all]": Text("git fetch --all") + Key("enter"),
+        "git log patch": Text("git log --oneline -p") + Key("enter"),
     }
     extras = [
         Dictation("text"),
@@ -236,7 +363,7 @@ class FuzzyFileRule(CompoundRule):
 
     def _process_recognition(self, node, extras):
         Key("space, c-t").execute()
-        Text(smash(extras["text"])).execute()
+        Text(smash_case(extras["text"])).execute()
 terminal_grammar.add_rule(FuzzyFileRule())
 
 
@@ -245,9 +372,9 @@ class FuzzyCDRule(CompoundRule):
     extras = [Dictation("text")]
 
     def _process_recognition(self, node, extras):
-        Text("cd ").execute()
-        Key("c-t").execute()
-        Text(smash(extras["text"])).execute()
+        query = smash_case(str(extras["text"]))
+        Text("cd; cd (fd | fzf --query %s)" % query).execute()
+        Key("enter").execute()
 terminal_grammar.add_rule(FuzzyCDRule())
 
 
@@ -256,9 +383,10 @@ class FuzzyEditRule(CompoundRule):
     extras = [Dictation("text")]
 
     def _process_recognition(self, node, extras):
-        Text("$EDITOR ").execute()
-        Key("c-t").execute()
-        Text(smash(extras["text"])).execute()
+        query = smash_case(str(extras["text"]))
+        Text("findedit %s)" % query).execute()
+        Key("enter").execute()
+
 terminal_grammar.add_rule(FuzzyEditRule())
 
 
@@ -268,6 +396,7 @@ class TmuxRule(MappingRule):
         "rename session <text>": Key("c-b, dollar") + Key("backspace:50") + Text("%(text)s") + Key("enter"),
         "terminal <n>": Key("c-b, %(n)d"),
         "(disconnect|detach) session": Key("c-b, d"),
+        "(jump|switch|select|choose) session": Key("c-b, w"),
     }
     extras = [
         Dictation("text"),
@@ -281,25 +410,30 @@ terminal_grammar.load()
 class WindowManagementRule(MappingRule):
     mapping = {
         "abort": Key("c-c"),
-        "desktop [<n>]": Key("w-%(n)d:4"),
-        "[focus] window down": Key("s-down"),
         "[focus] window up": Key("s-up"),
-        "[focus] window left": Key("s-left"),
         "[focus] window down": Key("s-down"),
-        "[focus] (frame down|next frame|other frame|frame)": Key("w-j"),
+        "[focus] window left": Key("s-left"),
+        "[focus] window right": Key("s-right"),
+        "[focus] frame up": Key("ca-up"),
+        "[focus] frame down": Key("ca-down"),
+        "[focus] frame left": Key("ca-left"),
+        "[focus] frame right": Key("ca-right"),
         "[focus] (next|other) (screen|monitor)": Key("cw-j"),
         "spank": Key("enter"),
-        "(twin|double|hard) spank": Key("enter/25:2"),
+        "(twin|double|hard) spank": Key("enter") + Pause("50") + Key("enter"),
         "yes": Key("y, enter"),
         "no": Key("n, enter"),
         "markdown bullet <text>": Key("space, asterisk, space") + Text("%(text)s") + Key("space"),
-        "browse <text>": Key("c-l") + Text("%(text)s") + Key("enter"),
-        "browse history <text>": Key("c-l") + Text("%(text)s") + Key("down"),
+        "soft string <text>": Key("quote") + Text("%(text)s") + Key("quote"),
         "(search back|retro search) <text>": Key("c-r") + Pause("50") + Text("%(text)s"),
         "hoover [<n>]": Key("a-backspace:%(n)d"),
         "slurp [<n>]": Key("a-backspace:%(n)d"),
         "maximise": Key("w-m"),
         "say clipboard": OriginalKey("cas-e"),
+        "go up": Key("up"),
+        "go down": Key("down"),
+        "page up": Key("pageup"),
+        "page down": Key("pagedown"),
     }
     extras = [
         Dictation("text"),
@@ -309,8 +443,22 @@ class WindowManagementRule(MappingRule):
         "n": 1,
     }
 
-wm_grammar = Grammar("window magagement")
+class SwitchAwesomeTagRule(CompoundRule):
+    spec = "(desktop|desk|tag) <n>"
+    extras = [Integer("n", 1, 9)]
+
+    def _process_recognition(self, node, extras):
+        # Focus Aenea client via AutoHotkey
+        OriginalKey("cas-a").execute()
+
+        # Switch tag
+        keystroke = "cas-%s" % extras["n"]
+        Key(keystroke).execute()
+
+
+wm_grammar = Grammar("window management")
 wm_grammar.add_rule(WindowManagementRule())
+wm_grammar.add_rule(SwitchAwesomeTagRule())
 wm_grammar.load()
 
 
